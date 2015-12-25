b4c0eda2cf8988a82c8ee9cfdec3377eabc91fad
beanstown106 <nbenis106@gmail.com>
2015-12-25 14:31:51 -0500

    bullhead product

--- products/nexus_bullhead.mk
+++ products/nexus_bullhead.mk
@@ -12,9 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq (nexus_angler,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_angler.mk
-endif
-ifeq (nexus_bullhead,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_bullhead.mk
-endif
+# Inherit AOSP device configuration for bullhead
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := nexus_bullhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD90M/3085278:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90M 3085278 release-keys"
