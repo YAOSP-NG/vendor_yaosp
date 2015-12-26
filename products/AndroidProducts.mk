337cb9a592b6e6c3e4c101f16989ee63d921248e
beanstown106 <nbenis106@gmail.com>
2015-12-26 08:57:22 -0500

    add shamu product, and stock bootanimations

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,3 +18,6 @@ endif
 ifeq (nexus_bullhead,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_bullhead.mk
 endif
+ifeq (nexus_shamu,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_shamu.mk
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo nexus_angler-user
 add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_shamu-user
