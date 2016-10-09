5008bc61b55a2bff2a8c1c414e3e115f959b221c
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-09 14:24:23 +0200

    Bump YAOSP version to v0.00 NBD90Z
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -18,9 +18,9 @@ include vendor/yaosp/configs/system_additions.mk
 # Add YAOSP version as build prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.yaosp.version=0.00 \
-    ro.yaosp.branch=7.0.0_r6 \
-    ro.yaosp.buildbase=NRD90U \
-    ro.yaosp.builddate=2016-09-30
+    ro.yaosp.branch=7.0.0_r14 \
+    ro.yaosp.buildbase=NBD90Z \
+    ro.yaosp.builddate=2016-10-09
 
 # Use ad-blocking hosts file
 PRODUCT_COPY_FILES += \
