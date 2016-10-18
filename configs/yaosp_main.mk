d74f4fb6591c6744a6d0731f7e89c9faba8ba0b5
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-18 14:26:13 +0200

    Bump YAOSP version to v0.05 NBD90Z
      - Display - Rotation : allow 0, 90, 180, 270 degrees & lockscreen rotation
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -17,10 +17,10 @@ include vendor/yaosp/configs/system_additions.mk
 
 # Add YAOSP version as build prop
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=0.04 \
+    ro.yaosp.version=0.05 \
     ro.yaosp.branch=7.0.0_r14 \
     ro.yaosp.buildbase=NBD90Z \
-    ro.yaosp.builddate=2016-10-17
+    ro.yaosp.builddate=2016-10-18
 
 # Use ad-blocking hosts file
 PRODUCT_COPY_FILES += \
