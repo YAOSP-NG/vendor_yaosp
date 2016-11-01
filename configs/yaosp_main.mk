8862a635ad50d2b36db3e60eabdd215e5009a443
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-11-01 10:37:14 +0100

    Bump YAOSP version to v0.11 NBD90Z
      - Notifications - Settings menu icon : notification light (per-app)
      - Notifications - Settings menu icon : battery light (separate color for slow/fast charging)
      - Security : Show/hide negociated charging current and voltage on lockscreen
      - show icon of package associated with Toast
      - battery-light : only show full when battery is done charging (instead of at 90%)
      - More - Mobile Networks - toggle to show/hide data connection in signal icon when roaming (instead of "R")
      - Phone - Settings - Sounds & vibration : in call vibration options
      - Phone - Settings - Calls : Flip to Mute/Reject Call
      - Phone - Settings - Calls : Proximity speaker
      - shamu: fix dt2w switch not working in display settings
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -17,10 +17,10 @@ include vendor/yaosp/configs/system_additions.mk
 
 # Add YAOSP version as build prop
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=0.10 \
+    ro.yaosp.version=0.11 \
     ro.yaosp.branch=7.0.0_r14 \
     ro.yaosp.buildbase=NBD90Z \
-    ro.yaosp.builddate=2016-10-29
+    ro.yaosp.builddate=2016-10-31
 
 # Use ad-blocking hosts file
 PRODUCT_COPY_FILES += \
