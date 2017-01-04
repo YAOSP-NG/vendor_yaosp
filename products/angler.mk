f654bac5da63ac93c3479c1845fb945fbffeab7a
beanstown106 <nbenis106@gmail.com>
2017-01-04 12:18:14 +0100

    update fingerprints: january security update

--- products/angler.mk
+++ products/angler.mk
@@ -28,8 +28,8 @@ PRODUCT_MANUFACTURER := Huawei
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
 
 # Addtional backup tools for dirty flash
 PRODUCT_COPY_FILES += \
