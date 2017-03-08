eb7f3f88482193c8eb682b4aecc98b26eaa5aaf8
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-03-08 20:49:27 +0100

    Bump YAOSP version to v1.10 N4F26T
      - rebased on android-7.1.1_r24 (N4F26T)
      - angler-kernel : rebased on android-msm-angler-3.10-nougat-mr1.1/android-7.1.1_r0.25
      - angler-kernel : Remove verifiedbootstate flag from /proc/cmdline (help pass safetynet)
      - bullhead-kernel : rebased on android-msm-bullhead-3.10-nougat-mr1.1/android-7.1.1_r0.47
      - bullhead-kernel : Remove verifiedbootstate flag from /proc/cmdline (help pass safetynet)
      - shamu: switch default governor to intellimm
      - shamu-kernel : rebased on android-msm-shamu-3.10-nougat-mr1.5/android-7.1.1_r0.49
      - shamu-kernel: add intellimm governor
      - hammerhead-kernel: intellimm - fix default optimal frequency setting
      - OMS/Substratum rootless support updated to OMS7-N @ 2017-03-08
        ThemeInterfacer updated to release 50
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -17,10 +17,10 @@ include vendor/yaosp/configs/system_additions.mk
 
 # Add YAOSP version as build prop
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=1.01 \
-    ro.yaosp.branch=7.1.1_r14 \
-    ro.yaosp.buildbase=N4F26O \
-    ro.yaosp.builddate=2017-02-19
+    ro.yaosp.version=1.10 \
+    ro.yaosp.branch=7.1.1_r24 \
+    ro.yaosp.buildbase=N4F26T \
+    ro.yaosp.builddate=2017-03-08
 
 # Include OMS (Substratum)
 PRODUCT_PROPERTY_OVERRIDES += \
